60692177b641cf76f129cd22b7da1dc48cf74863
Hoàng Hữu Hùng <116615229+hoanghungnoob@users.noreply.github.com>
2024-05-13 09:24:30 +0700

    Yum 92 story book (#9)
    * YUM-92 init storybook
    
    * YUM-92 component button

---
+++ src/components/client/button/Button.stories.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "./Button";
+
+export default {
+    title:'Button',
+    component:Button,
+}
+
+export const BookATable =()=><Button variant="btn-primary" >Book A Table</Button>
+
+export const ExploreMenu =()=><Button variant="btn-explore" >Explore Menu</Button>
+
+export const MoreAboutUs =()=><Button variant="btn-second" >More About Us</Button>
+
+export const ReadAllArticles =()=><Button variant="btn-primary" >Read All Articles</Button>
+
+export const Send =()=><Button variant="send active" >Send</Button>
+
