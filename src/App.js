02c715e113f2d5c23a86eb696d4af6b65e43a3c3
rinaksor <146155132+rinaksor@users.noreply.github.com>
2024-06-05 11:12:02 +0700

    Yum 84 UI shopping cart (#21)
    * YUM-84 ui shopping cart
    
    * YUM-84 ui update logo
    
    * YUM-84 ui shopping cart

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Footer from './components/Footer';
 import About from './pages/about';
 import Contact from './pages/contact';
 import Order from './pages/oder';
+import Cart from './pages/shoppingCart'
+import { ShoppingCart } from '@mui/icons-material';
 
 function App() {
   return (
@@ -22,6 +24,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/order" element={<Order />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
       <Footer />
     </div>
